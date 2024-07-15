5551374b817b08f6b73cb32a26c7d63be87e5fee
sumita <sumitabalouch@gmail.com>
2024-07-15 23:46:32 +0430

    added the htmk,css and js codes for first page

---
+++ script.js
@@ -0,0 +1,19 @@
+const hamburgerMenu = document.getElementById('hamburger-menu');
+const mobileMenu = document.getElementById('mobile-menu');
+const menuItems = document.getElementsByClassName('menu-item');
+
+hamburgerMenu.addEventListener('click', function() {
+  mobileMenu.classList.toggle('active');
+  for (let i = 0; i < sections.length; i++) {
+    sections[i].classList.toggle('hidden');
+  }
+});
+
+for (let i = 0; i < menuItems.length; i++) {
+  menuItems[i].addEventListener('click', function() {
+    mobileMenu.classList.remove('show');
+    for (let j = 0; j < sections.length; j++) {
+      sections[j].classList.add('hidden');
+    }
+  });
+}
