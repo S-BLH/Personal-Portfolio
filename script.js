db0a84859371e2681336e6f323353698a65f7c2b
sumita <sumitabalouch@gmail.com>
2024-07-18 20:35:16 +0430

    added new codes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const hamburgerMenu = document.getElementById('hamburger-menu');
 const mobileMenu = document.getElementById('mobile-menu');
 const menuItems = document.getElementsByClassName('menu-item');
+const sections = document.getElementsByClassName('section'); // Assuming you have sections to toggle
 
 hamburgerMenu.addEventListener('click', function() {
   mobileMenu.classList.toggle('active');
@@ -11,7 +12,7 @@ hamburgerMenu.addEventListener('click', function() {
 
 for (let i = 0; i < menuItems.length; i++) {
   menuItems[i].addEventListener('click', function() {
-    mobileMenu.classList.remove('show');
+    mobileMenu.classList.remove('active');
     for (let j = 0; j < sections.length; j++) {
       sections[j].classList.add('hidden');
     }
